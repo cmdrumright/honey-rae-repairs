f11a40f591eddb557d666e5cfc4ece6c1d21167e
cmdrumright <cdrumright@protonmail.com>
2026-01-06 10:47:23 -0600

    Added buttons to filter ticket list for emergency or all

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,32 @@ import "./app.css"
 
 export const App = () => {
     const [allTickets, setAllTickets] = useState([])
+    const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
+    const [filteredTickets, setFilteredTickets] = useState([])
 
     useEffect(() => {
         getAllTickets().then((ticketArray) => {
             setAllTickets(ticketArray)
-            console.log("tickets set!")
         })
     }, []) // only runs on initial render of component
 
+    useEffect(() => {
+        if (showEmergencyOnly) {
+            const emergencyTickets = allTickets.filter(ticket => ticket.emergency === true)
+            setFilteredTickets(emergencyTickets)
+        } else {
+            setFilteredTickets(allTickets)
+        }
+    }, [showEmergencyOnly, allTickets])
 
     return <div className="tickets-container">
         <h2>Tickets</h2>
+        <div>
+            <button className="filter-btn btn-primary" onClick={() => {setShowEmergencyOnly(true)}}>Emergency</button>
+            <button className="filter-btn btn-info" onClick={() => {setShowEmergencyOnly(false)}}>Show All</button>
+        </div>
         <article className="tickets">
-            {allTickets.map(ticket => {
+            {filteredTickets.map(ticket => {
                 return (
                     <section className="ticket" key={ticket.id}>
                         <header className="ticket-info">#{ticket.id}</header>
