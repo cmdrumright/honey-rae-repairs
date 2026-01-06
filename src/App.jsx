5c1595080db63445be242e4eb60e752a791d78ac
cmdrumright <cdrumright@protonmail.com>
2026-01-06 10:03:50 -0600

    Added component showing tickets added app.css with coppied css added services/ticketService to fetch tickets from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+import { getAllTickets } from "./services/ticketService.jsx"
+import "./app.css"
 
 export const App = () => {
-    const [count, setCount] = useState(0) // [stateVariable, setterFunction]
+    const [allTickets, setAllTickets] = useState([])
 
-	const handleBtnClick = () => {
-        setCount(count + 1)
-	}
-	return (
-		<>
-			<h1>Hello!</h1>
-			<div>This is amazing!</div>
-			<button className="btn-secondary" onClick={handleBtnClick}>Click me!</button>
-            <div>Count: {count}</div>
-		</>
-	)
+    useEffect(() => {
+        getAllTickets().then((ticketArray) => {
+            setAllTickets(ticketArray)
+            console.log("tickets set!")
+        })
+    }, []) // only runs on initial render of component
+
+
+    return <div className="tickets-container">
+        <h2>Tickets</h2>
+        <article className="tickets">
+            {allTickets.map(ticket => {
+                return (
+                    <section className="ticket" key={ticket.id}>
+                        <header className="ticket-info">#{ticket.id}</header>
+                        <div>{ticket.description}</div>
+                        <footer>
+                            <div>
+                                <div className="ticket-info">emergency</div>
+                                <div>{ticket.emergency ? "yes" : "no"}</div>
+                            </div>
+                        </footer>
+                    </section>
+                )
+            })}
+        </article>
+    </div>
 }
