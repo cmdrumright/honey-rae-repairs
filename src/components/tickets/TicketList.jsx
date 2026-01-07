0ad7c65a7a74deed6dd657e31e9c20cfd8d21615
cmdrumright <cdrumright@protonmail.com>
2026-01-07 10:44:55 -0600

    Added component for filter bar and moved filter buttons to it Added text input for filtering tickets by description

--- src/components/tickets/TicketList.jsx
+++ src/components/tickets/TicketList.jsx
@@ -3,11 +3,13 @@ import { getAllTickets } from "../../services/ticketService"
 
 import "./Tickets.css"
 import { Ticket } from "./Ticket"
+import { FilterBar } from "./FilterBar"
 
 export const TicketList = () => {
     const [allTickets, setAllTickets] = useState([])
     const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
     const [filteredTickets, setFilteredTickets] = useState([])
+    const [searchTerm, setSearchTerm] = useState("")
 
     useEffect(() => {
         getAllTickets().then((ticketArray) => {
@@ -24,12 +26,14 @@ export const TicketList = () => {
         }
     }, [showEmergencyOnly, allTickets])
 
+    useEffect(() => {
+        const foundTickets = allTickets.filter((ticket) => ticket.description.toLowerCase().includes(searchTerm.toLowerCase()))
+        setFilteredTickets(foundTickets)
+    }, [searchTerm, allTickets])
+
     return <div className="tickets-container">
         <h2>Tickets</h2>
-        <div>
-            <button className="filter-btn btn-primary" onClick={() => {setShowEmergencyOnly(true)}}>Emergency</button>
-            <button className="filter-btn btn-info" onClick={() => {setShowEmergencyOnly(false)}}>Show All</button>
-        </div>
+        <FilterBar setShowEmergencyOnly={setShowEmergencyOnly} setSearchTerm={setSearchTerm}/>
         <article className="tickets">
             {filteredTickets.map(ticketObj => {
                 return <Ticket ticket={ticketObj} key={ticketObj.id} />
