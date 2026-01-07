48771771233846a1080c45920e3c999efb320487
cmdrumright <cdrumright@protonmail.com>
2026-01-07 10:07:10 -0600

    Added service to get employees from API updated ticket service to embed assigned employees modified Ticket component to show assigned employee

--- src/components/tickets/Ticket.jsx
+++ src/components/tickets/Ticket.jsx
@@ -1,9 +1,32 @@
+import { useEffect, useState } from "react"
+import { getAllEmployees } from "../../services/employeeService"
+
 export const Ticket = ({ ticket }) => {
+    const [employees, setEmployees] = useState([])
+    const [assignedEmployee, setAssignedEmploy] = useState({})
+
+    useEffect(() => {
+        getAllEmployees().then((employeesArray) => {
+            setEmployees(employeesArray)
+        })
+    }, [])
+
+    useEffect(() => {
+        const foundEmployee = employees.find(
+            (employee) => employee.id === ticket.employeeTickets[0]?.employeeId
+        )
+        setAssignedEmploy(foundEmployee)
+    }, [employees, ticket])
+
     return (
-        <section className="ticket" key={ticket.id}>
+        <section className="ticket">
             <header className="ticket-info">#{ticket.id}</header>
             <div>{ticket.description}</div>
             <footer>
+                <div>
+                    <div className="ticket-info">assignee</div>
+                    <div>{assignedEmployee ? assignedEmployee.user?.fullName : "None"}</div>
+                </div>
                 <div>
                     <div className="ticket-info">emergency</div>
                     <div>{ticket.emergency ? "yes" : "no"}</div>
