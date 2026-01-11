d48516b0b414379ecdc76bda0ce6f76001bca421
cmdrumright <cdrumright@protonmail.com>
2026-01-10 21:29:47 -0600

    debugged code from laptop

--- src/components/employees/EmployeeList.jsx
+++ src/components/employees/EmployeeList.jsx
@@ -1,24 +1,25 @@
-import { useEffect, useSate } from "react"
+import { useEffect, useState } from "react"
 import { getStaffUsers } from "../../services/userService"
-import { User } from "../user/User"
+import { User } from "../users/User"
 import "./Employees.css"
 
 export const EmployeeList = () => {
     const [employees, setEmployees] = useState([])
 
     useEffect(() => {
-        getStaffUsers().then((res) => {
-            setEmployees(res.json())
+        getStaffUsers().then((users) => {
+            console.log(users)
+            setEmployees(users)
         })
     },[])
 
     return (
         <div className="employees">
-            employees.map((employee) => {
+            {employees.map((employee) => {
                 return (
-                    <User User={employee} key={employee.id} />
+                    <User user={employee} key={employee.id} />
                 )
-            }
+            })}
         </div>
     )
 }
