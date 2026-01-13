290624ed09dea185b9d5a5f4282e51ab9ed38911
cmdrumright <cdrumright@protonmail.com>
2026-01-13 14:34:49 -0600

    - Moved routes into Applications views - Added login and register routes - Added authorized component to app and made applicationViews a child of it - added state for currentUser and useEffect for getting currentUser from localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,23 @@
-import {Outlet, Route, Routes} from "react-router-dom"
+import { Route, Routes} from "react-router-dom"
 import "./app.css"
-import { NavBar } from "./components/nav/NavBar.jsx"
-import { CustomerList } from "./components/customers/CustomersList"
-import { EmployeeList } from "./components/employees/EmployeeList"
-import { TicketList } from "./components/tickets/TicketList"
-import { Welcome } from "./components/welcome/Welcome.jsx"
-import { CustomerDetails } from "./components/customers/CustomerDetails.jsx"
-import { EmployeeDetails } from "./components/employees/EmployeeDetails.jsx"
+import { Login } from "./components/auth/Login.jsx"
+import { Register } from "./components/auth/Register.jsx"
+import { Authorized } from "./views/Authorized.jsx"
+import { ApplicationViews } from "./views/ApplicationViews.jsx"
 
 export const App = () => {
     return (
         <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route
-                path="/"
+                path="*"
                 element={
-                    <>
-                        <NavBar />
-                        <Outlet />
-                    </>
-                } >
-                <Route index element={<Welcome />} />
-                <Route path="tickets" element={<TicketList />} />
-                <Route path="employees" element={<EmployeeList />} />
-                <Route path="customers">
-                    <Route index element={<CustomerList />} />
-                    <Route path=":customerId" element={<CustomerDetails />} />
-                </Route>
-                <Route path="employees">
-                    <Route index element={<EmployeeList />} />
-                    <Route path=":employeeId" element={<EmployeeDetails />} />
-                </Route>
-            </Route>
+                    <Authorized>
+                        <ApplicationViews />
+                    </Authorized>
+                }
+            />
         </Routes>
     )
 }
--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,3 +1,43 @@
+import { Outlet, Route, Routes} from "react-router-dom"
+import { NavBar } from "../components/nav/NavBar.jsx"
+import { CustomerList } from "../components/customers/CustomersList"
+import { EmployeeList } from "../components/employees/EmployeeList"
+import { TicketList } from "../components/tickets/TicketList"
+import { Welcome } from "../components/welcome/Welcome.jsx"
+import { CustomerDetails } from "../components/customers/CustomerDetails.jsx"
+import { EmployeeDetails } from "../components/employees/EmployeeDetails.jsx"
+import { useEffect, useState } from "react"
+
 export const ApplicationViews = () => {
-  return <></>
+    const [currentUser, setCurrentUser] = useState({})
+    
+    useEffect(() => {
+        const localHoneyUser = localStorage.getItem("honey_user")
+        const honeyUserObject = JSON.parse(localHoneyUser)
+
+        setCurrentUser(honeyUserObject)
+    }, [])
+
+  return <Routes>
+    <Route
+        path="/"
+        element={
+            <>
+                <NavBar />
+                <Outlet />
+            </>
+        } >
+        <Route index element={<Welcome />} />
+        <Route path="tickets" element={<TicketList />} />
+        <Route path="employees" element={<EmployeeList />} />
+        <Route path="customers">
+            <Route index element={<CustomerList />} />
+            <Route path=":customerId" element={<CustomerDetails />} />
+        </Route>
+        <Route path="employees">
+            <Route index element={<EmployeeList />} />
+            <Route path=":employeeId" element={<EmployeeDetails />} />
+        </Route>
+    </Route>
+  </Routes>
 }
